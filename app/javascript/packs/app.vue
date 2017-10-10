ce2e983c1e71cea3ab3d4b83ceba11d687a44094
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-10-10 17:30:09 +0900

    make vue component test environment

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <MessageList :messages="messages"/>
   </div>
 </template>
 
 <script>
+import MessageList from './components/MessageList'
+
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+  name: 'app',
+  data: () => ({ messages: ['Hey John', 'Howdy Paco'] }),
+  components: {
+    MessageList
   }
 }
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
